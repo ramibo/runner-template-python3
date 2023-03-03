308b62bfa22c31ce8872668def5eeb80466b5695
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2023-03-03 14:55:21 +0200

    updated boilerplate template

--- template/runner-template-python3/function/main_store.py
+++ template/runner-template-python3/function/main_store.py
@@ -1,24 +1,34 @@
-
 import sys
 from pathlib import Path
 from pydantic import BaseModel
+from typing import Optional, List, Dict, Any
 import kubiya
 
-action_store = kubiya.ActionStore("sample-store", "0")
+# This is the action store object
+# It is used to define the action store name and version
+# It will get passed to the Kubiya platform and used to identify the action store
+action_store = kubiya.ActionStore("terraform", "0")
 
+# This is a simple action that returns a string
+# It is not validated, so you can pass anything as an input
 @action_store.kubiya_action(validate_input=True)
-def simple_action(a: str):
-    "returns bar"
-    return f"hi {a} from simple_action"
+def simple_action(input):
+    """returns foo"""
+    return "Hello from simple_action! Recieved input: " + str(input)
 
-
+# This is an example model, you can use it to validate your input
+# And build amazing user interfaces in the Kubiya no-code platform
 class ExampleModel(BaseModel):
     """used for type hints, automatic decumentation and validation"""
-    your_string: str
-    your_int: int
+    required_string: str
+    optional_string: Optional[str] = None
+    required_int: int
+    optional_int: Optional[int] = None
 
+# This is a simple action that requires a model as input
+# It is validated, so you can only pass a model that matches the ExampleModel
+# It will also be documented in the Kubiya no-code platform
 @action_store.kubiya_action(validate_input=True)
 def action_with_model(user: ExampleModel) -> str:
     """returns baz"""
-    return f"{user.get('your_string')} | {user.get('your_int')} | {user.get('email')} from baz"
-
+    return "Hello from action_with_model! Recieved input: " + str(user)
