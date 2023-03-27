a35b9d623b93fbbba7c33c2d0ff5f306ed830f78
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2023-03-27 10:56:43 +0300

    removed unneeded dependency

--- template/runner-template-python3/function/actions/__init__.py
+++ template/runner-template-python3/function/actions/__init__.py
@@ -2,7 +2,6 @@ import time
 from os import environ
 from pprint import pprint
 
-# from sh import gcloud
 from kubiya import ActionStore, get_secret
 
 actionstore = ActionStore("new-action-store", "0.1.0")
